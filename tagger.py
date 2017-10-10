2c41923304e11c6c3df97db57733edd981545c27
jaulie <jg5059@nyu.edu>
2017-10-09 23:35:05 -0400

    better score

--- tagger.py
+++ tagger.py
@@ -28,18 +28,15 @@ for word in corpus:
 	if not word:
 		output.write('\n')
 		previous = '*start_end*'
-		print(previous)
 		continue
 	# Handle OOV
 	if word.lower() not in word_tags:
 		output.write('{}\tOOV\n'.format(word))
-		print(OOV)
 		continue
 	# Check if word corresponds to one or multiple tags
 	if len(word_tags[word.lower()]) == 1:
 		output.write('{}\t{} \n'.format(word, word_tags[word.lower()][0]))
 		previous = word_tags[word.lower()][0]
-		print(previous)
 	# If the word corresponds to multiple tags, find the probability
 	# of each tag by iterating through each of the corresponding tags
 	else:
@@ -47,27 +44,25 @@ for word in corpus:
 			# Iterates through the tags associated with that word
 			tag_probabilities = pos_prob_table[previous]
 			word_emission = word_likelihood[tag]
-			#if tag in tag_probabilities: 
-			tag_prob = tag_probabilities[tag] #probability of the tag
-			print(tag_prob)
-			#else: tag_prob = 0.0001
+			if tag in tag_probabilities: tag_prob = tag_probabilities[tag] #probability of the tag
+			else: tag_prob = 0.0001
 			word_prob = word_emission[word.lower()] #probability that that POS tags the word
 			# Loop through each of the preceding word's possible tags
-			for each in previous_prob: 
-				print('Previous prob', previous_prob[each])
+			for each_tag in previous_prob: 
 				# Calculate the probability based on probability of previous tag
-				prob = previous_prob[each] * tag_prob * word_prob 
+				prob = previous_prob[each_tag] * tag_prob * word_prob 
 				# Place it in the Viterbi dictionary
-				viterbi[each] = prob 
+				viterbi[each_tag] = prob 
 			# Then get the maximum probability from Viterbi, this is the final probability of this tag corresponding to word
 			max_prob = max(viterbi.values())
 			final[tag] = max_prob
+			viterbi = {}
 		previous_prob = final
+		final = {}
 		# Get the tag that is most probable for this word
 		most_prob_tag = max(previous_prob, key = previous_prob.get)
 		output.write('{}\t{}\n'.format(word, most_prob_tag))
 		previous = most_prob_tag
-		print(previous)
 
 
 
